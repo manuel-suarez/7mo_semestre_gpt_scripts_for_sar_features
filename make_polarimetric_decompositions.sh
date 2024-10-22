b859868e905dfb07b11432657d0d4e9896c36e45
Arturo Suárez <manuel.suarez@cimat.mx>
2024-10-22 14:56:34 -0600

    Delete temporary files

--- make_polarimetric_decompositions.sh
+++ make_polarimetric_decompositions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gpt=~/tools/esa-snap8.0/bin/gpt
+gpt=~/tools/esa-snap10.0/bin/gpt
 base_path=$1
 source=$2
 dest=$3
@@ -69,3 +69,26 @@ if ! test -f $base_path/$temp/$name/pol_11.dim; then
   $gpt scripts/polarimetric_11.xml -SsourceProduct=$base_path/$temp/$name/pol_10.dim -t $base_path/$temp/$name/pol_11.dim
 fi
 # $gpt scripts/sar_export_to_tif.xml -SsourceProduct=dataset-sentinel/temp/$NAME.dim -t dataset-sentinel/GRD_tif/$NAME.tif
+# Remove temporary files 01-09
+#
+for step in 01 02 03 04 05 06 07 08 09
+do
+  if [[ $step == "02" || $step == "03" || $step == "04" ]]; then
+    for n in 1 2 3
+    do
+      if test -d $base_path/$temp/$name/pol_${step}_iw${n}.data; then
+        rm -rf $base_path/$temp/$name/pol_${step}_iw${n}.data
+      fi
+      if test -f $base_path/$temp/$name/pol_${step}_iw${n}.dim; then
+        rm $base_path/$temp/$name/pol_${step}_iw${n}.dim
+      fi
+    done
+  else
+    if test -d $base_path/$temp/$name/pol_${step}.data; then
+      rm -rf $base_path/$temp/$name/pol_${step}.data
+    fi
+    if test -f $base_path/$temp/$name/pol_${step}.dim; then
+      rm $base_path/$temp/$name/pol_${step}.dim
+    fi
+  fi
+done
