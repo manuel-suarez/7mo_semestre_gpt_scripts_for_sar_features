edae46724cac5f70b37daec8ad631c79d13bacf3
Arturo Suárez <manuel.suarez@cimat.mx>
2025-01-28 13:33:17 -0600

    Simplify filenames

--- read-wind-csv.py
+++ read-wind-csv.py
@@ -6,13 +6,14 @@ import geopandas as gpd
 from skimage.io import imsave
 from rasterio.features import rasterize
 from scipy.interpolate import griddata
+from matplotlib import pyplot as plt
 
 fname = "ASA_IMP_1PNESA20050425_075225_000000182036_00393_16479_0000"
 
 base_path = os.path.expanduser("~")
 data_path = os.path.join(base_path, "data", "cimat", "dataset-cimat", "temp4")
 prod_path = os.path.join(data_path, fname)
-tiff_path = os.path.join(data_path, fname + ".tif")
+tiff_path = os.path.join(data_path, fname, "image.tif")
 
 wind_path = os.path.join(prod_path, "wind_07.data", "vector_data")
 wind_input_file = os.path.join(wind_path, "WindField.csv")
@@ -24,6 +25,8 @@ with rasterio.open(tiff_path) as tiff_file:
     sar_image = tiff_file.read(1)
     sar_transform = tiff_file.transform
     sar_shape = sar_image.shape
+
+print("SAR image shape: ", sar_image.shape, sar_image.dtype)
 
 # Open file to remove # characters and :Datatype marks
 print("Open WindField CSV")
@@ -62,19 +65,29 @@ grid_points = np.column_stack([lon.flatten(), lat.flatten()])
 print("Interpolate wind data")
 wind_raster = rasterize(
     zip(points, speed_values),
-    out_shape=(sar_shape[1], sar_shape[0]),
+    out_shape=(sar_shape[0], sar_shape[1]),
     transform=sar_transform,
     fill=0,
 )
+w_min = np.min(wind_raster)
+w_max = np.max(wind_raster)
 print(
     "wind raster result: ",
     wind_raster.shape,
     wind_raster.dtype,
-    np.min(wind_raster),
-    np.max(wind_raster),
+    w_min,
+    w_max,
 )
 
 # Save wind field image
 print("Save windfield output image")
-imsave(os.path.join(prod_path, fname + "_wind.tif"), wind_raster)
+imsave(os.path.join(data_path, fname, "wind.tif"), wind_raster)
+
+# Make figure
+fig, axs = plt.subplots(1, 2, figsize=(14, 6))
+axs[0].imshow(sar_image, cmap="gray")
+axs[1].imshow(wind_raster, cmap="viridis", vmin=w_min, vmax=w_max)
+plt.savefig(os.path.join(data_path, fname, "figure.png"))
+plt.close()
+
 print("Done!")
